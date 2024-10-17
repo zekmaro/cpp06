34f25e9937855f3338553b8916a83f925242b624
Andrey Arama <anarama@c3r4p6.42vienna.com>
2024-10-17 19:03:27 +0200

    saving changes

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -6,12 +6,56 @@
 /*   By: anarama <anarama@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 14:43:50 by anarama           #+#    #+#             */
-/*   Updated: 2024/10/17 14:54:50 by anarama          ###   ########.fr       */
+/*   Updated: 2024/10/17 17:50:25 by anarama          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 
-void ScalarConverter::convert(std::string str) {
+/* EXCEPTIONS */
+
+const char* ScalarConverter::EmptyStringException::what() const throw() {
+	return ("Invalid input\nEmpty string found!");
+}
+
+const char* ScalarConverter::MultipleCharsException::what() const throw() {
+	return ("Invalid input\nMultiple chars found!");
+}
+
+
+/* MEMBER FUNCTIONS */
+
+bool hasChar( std::string str ) {
+	int i = 0;
+	while (str[i]) {
+		if (std::isalpha(str[i]))
+			return true;
+		i++;
+	}
+	return false;
+}
+
+void	convertChar( char c ) {
 	
+}
+
+void	convertNum( std::string str ) {
+	
+}
+
+void ScalarConverter::convert( std::string str ) {
+	if (str.empty()) {
+		throw ScalarConverter::EmptyStringException();
+	}
+	if (hasChar(str)) {
+		if (str.length() > 1) {
+			throw ScalarConverter::MultipleCharsException();
+		}
+		else {
+			convertChar(str[0]);
+		}
+	}
+	else {
+		convertNum(str);
+	}
 }
--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -6,13 +6,14 @@
 /*   By: anarama <anarama@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 14:43:53 by anarama           #+#    #+#             */
-/*   Updated: 2024/10/17 14:48:43 by anarama          ###   ########.fr       */
+/*   Updated: 2024/10/17 17:40:38 by anarama          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SCALARCONVERTER_HPP
 # define SCALARCONVERTER_HPP
 
+# include <exception>
 # include <string>
 
 class ScalarConverter {
@@ -23,7 +24,18 @@ class ScalarConverter {
 		~ScalarConverter( void );
 
 	public:
-		static void convert( std::string str );	
+		static void convert( std::string str );
+	
+		class EmptyStringException: public std::exception {
+			public:
+				const char* what() const throw();
+		};
+
+		class MultipleCharsException: public std::exception {
+			public:
+				const char* what() const throw();
+		};
+		
 };
 
 #endif // SCALARCONVERTER_HPP
